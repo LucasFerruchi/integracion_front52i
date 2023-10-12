719c74f108339f418fda381f19294ea136eabe82
LucasFerruchi <lucasferruchi05@gmail.com>
2023-10-12 10:56:53 -0300

    config categoriaApi

--- src/helpers/categoriaApi.js
+++ src/helpers/categoriaApi.js
@@ -1,101 +1,16 @@
 const url = "http://localhost:3000/api/categorias";
 const token = JSON.parse(localStorage.getItem("token"));
-const limite = 6;
 
-export const getCategorias = async (desde = 0) => {
-  try {
-    const resp = await fetch(url + "?limite=" + limite + "&desde=" + desde, {
-      method: "GET",
-
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        "x-token": token,
-      },
-    });
-    const data = await resp.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    throw new Error("No se pudo obtener la info");
-  }
-};
+export const getCategorias = async (desde = 0) => {};
 
 //Traer categorías por el id
-export const getCategoriaById = async (id) => {
-  try {
-    const resp = await fetch(url + "/" + id, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        "x-token": token,
-      },
-    });
-    const data = await resp.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-    throw new Error("No se pudo obtener la info");
-  }
-};
+export const getCategoriaById = async (id) => {};
 
 //crear categoria
-export const crearCategoria = async (datos) => {
-  try {
-    const resp = await fetch(url, {
-      method: "POST",
-      body: JSON.stringify(datos),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        "x-token": token,
-      },
-    });
-
-    const data = await resp.json();
-
-    return data;
-  } catch (error) {
-    console.log(error);
-    return { msg: "No se conectó con backend" };
-  }
-};
+export const crearCategoria = async (datos) => {};
 
 //Actualizar Categoría
-export const actualizarCategoria = async (id, datos) => {
-  try {
-    const resp = await fetch(url + "/" + id, {
-      method: "PUT",
-      body: JSON.stringify(datos),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        "x-token": token,
-      },
-    });
-
-    const data = await resp.json();
-
-    return data;
-  } catch (error) {
-    console.log(error);
-    return { msg: "No se conectó con backend" };
-  }
-};
+export const actualizarCategoria = async (id, datos) => {};
 
 //Borrar Categoría
-export const borrarCategoria = async (id) => {
-  try {
-    const resp = await fetch(url + "/" + id, {
-      method: "DELETE",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-        "x-token": token,
-      },
-    });
-
-    const data = await resp.json();
-
-    return data;
-  } catch (error) {
-    console.log(error);
-    return { msg: "No se conectó con backend" };
-  }
-};
+export const borrarCategoria = async (id) => {};
